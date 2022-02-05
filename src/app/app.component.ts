1bcd8ddbec1a1067c64d92e123fbcf473192d985
bksrinu546@gmail.com <bksrinu546@gmail.com>
2022-02-05 12:17:35 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularbootcamp';
+  includeLetters=false;
+  includeNumbers=false;
+  includeSymbols=false;
+  password='';
+  length=0;
+
+  constructor(){
+    this.password='';
+  }
+  onButtonClick(){
+    console.log(`
+    About to generate a password with the following:
+    Includes letters: ${ this.includeLetters }
+    Includes numbers: ${ this.includeNumbers }
+    Includes symbols: ${ this.includeSymbols }
+
+    `);
+
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+    let validchars='';
+    if(this.includeLetters){
+      validchars +=letters;
+    }
+    if(this.includeNumbers){
+      validchars += numbers;
+    }
+    if(this.includeSymbols){
+      validchars += symbols;
+    }
+    let generatedPassword='';
+    for(let i=0;i< this.length;i++){
+      const index = Math.floor(Math.random()*validchars.length);
+      generatedPassword += validchars[index];
+    }
+    this.password= generatedPassword;
+  }
+  onChangeUseLetters(){
+    this.includeLetters= !this.includeLetters;
+  }
+  onChangeUseNumbers(){
+    this.includeNumbers= !this.includeNumbers;
+  }
+  onChangeUseSymbols(){
+    this.includeSymbols= !this.includeSymbols;
+  }
+  onChangeLength(value:string){
+    const parsedValue = parseInt(value);
+    this.length=0;
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
+  }
 }
